68f68b132f413fd2b0d71cd0b850c572b505d6f9
qianwanyu <731842876@qq.com>
2022-08-19 19:18:27 +0800

    修改main

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <vector>
 
 #include "Core/DrawLine.h"
+#include "Core/Model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
@@ -18,16 +19,33 @@ const TGAColor blue = TGAColor(0, 0, 255, 255);
 using namespace std;
 
 int main() {
-    vector<vector<int>> PosValue = {{13, 20, 80, 40}, {20, 13, 30, 90}, {80, 40, 13, 20}};
-    vector<TGAColor> colors = {white, red, blue};
-    TGAImage image(100, 100, TGAImage::RGB);
-    int i=0;
-    for (auto& values : PosValue) {
-        DrawLine_V3(values[0], values[1], values[2], values[3], image, colors[i++]);
-    }
+    // 点绘制
+    // TGAImage image(100, 100, TGAImage::RGB);
+    // image.set(52, 41, white);
+    // image.flip_vertically();  // i want to have the origin at the left bottom corner of the image
+    // image.write_tga_file("output.tga");
 
+    //线条绘制
+    // vector<vector<int>> PosValue = {{13, 20, 80, 40}, {20, 13, 30, 90}, {80, 40, 13, 20}};
+    // vector<TGAColor> colors = {white, red, blue};
+    // TGAImage image(100, 100, TGAImage::RGB);
+    // int i=0;
+    // for (auto& values : PosValue) {
+    //     DrawLine_V4(values[0], values[1], values[2], values[3], image, colors[i++]);
+    // }
+
+    // image.flip_vertically();  // i want to have the origin at the left bottom corner of the image
+    // image.write_tga_file("output.tga");
+
+    TGAImage image(800, 800, TGAImage::RGB);
+    Model m("../obj/head.obj");
+    for (int i = 0; i < m.VertsSize(); i++) {
+        Vec3f v = m.Vert(i);
+        image.set(v.x, v.y, white);
+    }
     image.flip_vertically();  // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
+    
 
     return 0;
 }
