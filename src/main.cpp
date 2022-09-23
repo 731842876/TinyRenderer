98b4c74196ce74094322c60c6990cb11a86bba5d
qianwanyu <731842876@qq.com>
2022-09-23 19:08:26 +0800

    调用实例

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,13 @@
 #include "Core/DrawLine.h"
 #include "Core/Model.h"
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
-const TGAColor blue = TGAColor(0, 0, 255, 255);
+const TGAColor green = TGAColor(160, 191, 124, 255);
+const TGAColor red = TGAColor(248, 147, 29, 255);
+const TGAColor blue = TGAColor(147, 224, 255, 255);
 
 using namespace std;
 
-int main() {
+void s1() {
     // 点绘制
     // TGAImage image(100, 100, TGAImage::RGB);
     // image.set(52, 41, white);
@@ -47,12 +47,44 @@ int main() {
             int y0 = (v0.y + 1.) * 800 / 2.;
             int x1 = (v1.x + 1.) * 800 / 2.;
             int y1 = (v1.y + 1.) * 800 / 2.;
-            DrawLine_V4(x0, y0, x1, y1, image, white);
+            DrawLine_V4(x0, y0, x1, y1, image, green);
         }
     }
 
-    image.flip_vertically();  // i want to have the origin at the left bottom corner of the image
+    image.flip_vertically();
     image.write_tga_file("output.tga");
 
-    return 0;
+    return;
+}
+
+void TriangleDrawing(vector<Vec2i> Triangle, TGAImage& res) {
+    vector<TGAColor> colors = {green, red, blue};
+
+    for (int i = 0; i < 3; i++) {
+        Vec2i v0 = Triangle[i];
+        Vec2i v1 = Triangle[(i + 1) % 3];
+        DrawLine_V4(v0.x, v0.y, v1.x, v1.y, res, colors[i]);
+    }
+}
+
+void s2() {
+    TGAImage image(400, 400, TGAImage::RGB);
+
+    vector<vector<Vec2i>> Triangles = {{{10, 70}, {50, 160}, {70, 80}}};
+    for (auto& Triangle : Triangles) {
+        TriangleDrawing(Triangle, image);
+        for (auto& point : Triangle) {
+            cout << point.x << " " << point.y << endl;
+        }
+        cout << endl;
+    }
+
+    image.flip_vertically();
+    image.write_tga_file("output.tga");
+
+    return;
+}
+
+int main() {
+    s2();
 }
